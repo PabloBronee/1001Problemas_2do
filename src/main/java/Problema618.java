2836aa75f4505e661a5faa82abc9e6dc5636a6f3
Pablo Bronee <pablobronnnee2003@gmail.com>
2025-05-11 08:43:20 -0300

    Problema 618 realizado

--- src/main/java/Problema618.java
+++ src/main/java/Problema618.java
@@ -1,2 +1,38 @@
 public class Problema618 {
+    public static void main(String[] args) {
+        class Empleado{
+            //Atributos
+            String nombre;
+            double salario;
+            int edad;
+
+            //Constructor
+            Empleado(String nombre, double salario, int edad){
+                this.nombre = nombre;
+                this.salario = salario;
+                this.edad = edad;
+            }
+
+            public boolean esElegible(){
+                if (edad > 17 && salario > 0.0){
+                    System.out.println(nombre + " es mayor de edad y tiene salario asignado, es elegible");
+                    return true;
+                }
+                else{
+                    System.out.println(nombre + " es menor de edad, o NO tiene salario asignado, no es elegible");
+                    return false;
+                }
+
+            }
+        }
+        //Creamos dos objetos diferentes, para dar los dos resultados del metodo "esElegible"
+        Empleado empleado1 = new Empleado("Pablo",23.000,21);
+        Empleado empleado2 = new Empleado("Lucas",0.0,15);
+
+        //Comprobamos el metodo con ambos objetos
+        empleado1.esElegible();
+        empleado2.esElegible();
+
+    }
 }
+
